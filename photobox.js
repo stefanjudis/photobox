a7a5c17697d59d571ec63a108ae507e3d19d58de
stefan judis <stefanjudis@gmail.com>
2014-03-05 23:41:37 +0100

    copy of grunt-photobox files - initial move over

---
+++ photobox.js
@@ -0,0 +1,36 @@
+/*
+ * grunt-photobox
+ * https://github.com/stefan/grunt-photoBox
+ *
+ * Copyright (c) 2013 stefan judis
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+
+module.exports = function( grunt ) {
+  grunt.registerMultiTask(
+    'photobox',
+    'Take screenshots for different url\'s and compare them.',
+    function() {
+      var done     = this.async(),
+          options  = this.options( {
+            indexPath                     : 'photobox/',
+            javascriptEnabled             : true,
+            localToRemoteUrlAccessEnabled : true,
+            loadImages                    : true,
+            password                      : '',
+            screenSizes                   : [ '800' ],
+            template                      : 'canvas',
+            userAgent                     : 'Photobox',
+            userName                      : '',
+            urls                          : [ 'http://4waisenkinder.de' ]
+          } ),
+          Photobox = require( './lib/photobox' ),
+          pb       = new Photobox( grunt, options, done );
+
+      pb.startPhotoSession();
+    }
+  );
+};
